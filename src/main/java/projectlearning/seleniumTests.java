589fd422fff9ddd8e8058c77ddfae03a2f11c4b0
sarath152 <sarathcharm15@gmail.com>
2022-04-25 00:42:43 +0530

    these are copied

---
+++ src/main/java/projectlearning/seleniumTests.java
@@ -0,0 +1,18 @@
+package projectlearning;
+
+import org.testng.annotations.Test;
+
+public class seleniumTests {
+	@Test
+	public void structuuu(){
+
+	System.out.println("structuuu");
+	}
+
+	@Test
+	public void structrrr(){
+
+	System.out.println("structrrr");
+	}
+
+}
