7270cc6e113ca22dc2ad4dafa134fbc821df2ff2
sarath152 <sarathcharm15@gmail.com>
2022-04-24 23:40:14 +0530

    updated1

---
+++ src/test/java/projectlearning/ApiumTest.java
@@ -0,0 +1,17 @@
+package projectlearning;
+
+import org.testng.annotations.Test;
+
+public class ApiumTest {
+	@Test
+	public void structee(){
+
+	System.out.println("structee");
+	}
+
+	@Test
+	public void structraa(){
+
+	System.out.println("structraa");
+	}
+}
---
+++ src/test/java/projectlearning/seleniumTest.java
@@ -0,0 +1,19 @@
+package projectlearning;
+
+import org.testng.annotations.Test;
+
+
+public class seleniumTest {
+	@Test
+	public void struct(){
+
+	System.out.println("struct");
+	}
+
+	@Test
+	public void structr(){
+
+	System.out.println("structr");
+	}
+
+}
